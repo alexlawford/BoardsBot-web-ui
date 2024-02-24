df0ac7768d2cbe37e68453c1748c5163388c0add
Alexander Browne <alexlawford@icloud.com>
2024-02-24 20:40:02 +0800

    Working beam calling page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+let responseText;
+let requestSent;
+let taskId;
+let taskResponse;
+
+function sendRequest() {
+    requestSent = true;
+
+    fetch("https://sqwi2.apps.beam.cloud", {
+    "method": "POST",
+    "headers": {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Authorization": "Basic YjEyMTJjOTI5NGJlZWUyYjY1MmEyYTkxNzVkMmQwNTU6YzFhZGU0YWY5M2YxNjFlNTcyZDM4MGMxMjA4M2JlZjc=",
+        "Content-Type": "application/json"
+    },
+    "body": JSON.stringify({})
+    })
+    .then(response => response.text())
+    .then(text => {
+        taskId = JSON.parse(text)['task_id'];
+        responseText = "Beam.cloud responded with Task ID " + taskId;
+    })
+    .catch(err => console.error(err));
+}
+
+function checkStatus() {
+    taskResponse = "Checking...";
+    fetch("https://api.beam.cloud/v1/task/" + taskId + "/status/", {
+    "method": "GET",
+    "headers": {
+        "Authorization": "Basic YjEyMTJjOTI5NGJlZWUyYjY1MmEyYTkxNzVkMmQwNTU6YzFhZGU0YWY5M2YxNjFlNTcyZDM4MGMxMjA4M2JlZjc=",
+        "Content-Type": "application/json"
+    },
+    })
+    .then(response => response.text())
+    .then(text => {
+        let status = JSON.parse(test)['status'];
+        taskResponse = "Status is: " + status;
+    })
+    .catch(err => console.error(err));
+}
+</script>
+
+<h1>Welcome to BoardsBot</h1>
+
+{#if requestSent}
+    {#if responseText}
+        <p>{responseText} <button on:click={checkStatus}>Check task status</button></p>
+    {:else}
+        <p>Sending request to beams.cloud...</p>
+    {/if}
+    {#if taskResponse}
+        <p>{taskResponse}</p>
+    {/if}
+{:else}
+    <p><button on:click={sendRequest}>Send request</button></p>
+{/if}
